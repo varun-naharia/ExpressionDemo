177140bec73d021658db94ae05a8d8c9651697b8
Varun Naharia <v.naharia@logictrixtech.com>
2017-12-21 18:57:24 +0530

    First Commit

--- ExpressionDemo/ViewController.swift
+++ ExpressionDemo/ViewController.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet weak var inputField: UITextView!
+    @IBOutlet weak var outputView: UILabel!
+    private var output = NSMutableAttributedString()
+    @IBOutlet var picker: UIPickerView!
+    @IBOutlet var toolbar: UIToolbar!
+    @IBOutlet weak var toolbarTitle: UIBarButtonItem!
+    var arrVariable:[String] = []
+    
+    private func addOutput(_ string: String, color: UIColor) {
+        let text = NSAttributedString(string: string + "\n\n", attributes: [
+            NSAttributedStringKey.foregroundColor: color,
+            NSAttributedStringKey.font: outputView.font!,
+            ])
+        output.replaceCharacters(in: NSMakeRange(0, 0), with: text)
+        outputView.attributedText = output
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        inputField.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
+        output.append(outputView.attributedText!)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +39,46 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func textViewDidChange(_ textView: UITextView) {
+        if let text = inputField.text, text != "" {
+            do {
+                let result = try Expression(text).evaluate()
+                addOutput(String(format: "= %g", result), color: .black)
+            } catch {
+                addOutput("\(error)", color: .red)
+            }
+        }
+    }
 }
+
+extension ViewController:UIPickerViewDelegate, UIPickerViewDataSource
+{
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        toolbarTitle.title = "Variable"
+        return arrVariable.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return arrVariable[row]
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        //        expressionBuilderView.addFunction(functionName: arrFunctions[row]["FunctionName"] as! String, minArg: arrFunctions[row]["MinArg"] as! Int, maxArg: arrFunctions[row]["MaxArg"] as! Int)
+    }
+    func addDoneButtonOnKeyboard()
+    {
+        expressionBuilderView.expressionField.inputAccessoryView = toolbar
+    }
+    
+    func removeDoneButtonOnKeyboard()
+    {
+        expressionBuilderView.expressionField.inputAccessoryView = nil
+    }
+    
+}
+
 
